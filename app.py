92d41afedc4d4e43671b8cb574155db7538ec190
Bolu Akinlawon <78508788+BoluAkinlawon@users.noreply.github.com>
2025-09-02 11:18:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+pip install python-dotenv
+
 from flask import Flask, request, jsonify
 import hashlib
 import os
@@ -31,3 +33,4 @@ def handle_notification():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
